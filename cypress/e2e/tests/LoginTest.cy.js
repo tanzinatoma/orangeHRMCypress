524bca677c7863087a8ed4b991303e68d3834e08
Syeda Tanzina Farhin Toma <farhin.toma@welldev.io>
2024-10-02 17:01:14 +0600

    POM Structure Basic

--- cypress/e2e/tests/LoginTest.cy.js
+++ cypress/e2e/tests/LoginTest.cy.js
@@ -1,8 +1,9 @@
+import { LoginPage } from "../pages/LoginPage";
 it('User Logs into the system successfully', ()=>{
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    cy.get('[name="username"]').type("Admin");
-    cy.get('[name="password"]').type("admin123{enter}");
-    if(cy.url().should('include', '/dashboard')){
-        cy.log('User successfully logged in');
-    }
+    
+    
+    // if(cy.url().should('include', '/dashboard')){
+    //     cy.log('User successfully logged in');
+    // }
 })
